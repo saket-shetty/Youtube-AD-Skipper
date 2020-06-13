753357bcae0e5fbef0482f6c4485da8af54fe0bb
Saket D Shetty <shettysaket05@gmail.com>
2020-06-13 18:19:16 +0530

    cleaned code

--- content.js
+++ content.js
@@ -1,16 +1,10 @@
-var emptychar = '';
-var date = new Date();
-var nowtime = date.getTime();
-
-console.log('running script......')
+console.log('running script......');
 
 //Call skipfunction in every 6 second
-setInterval(clickskip, 6000)
-
+setInterval(clickskip, 6000);
 
 //clicks youtubes skip button by it's class
 //usually after first skip it will skip the add before the 5 second
 function clickskip(){
-  console.log('plipplop');
   document.querySelector('.ytp-ad-skip-button.ytp-button').click();
 }
